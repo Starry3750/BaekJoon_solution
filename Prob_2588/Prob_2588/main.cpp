ef848d0afc2f0e47f5907f7ecbe743b8f7b91364
Starry3750 <49588390+Starry3750@users.noreply.github.com>
2020-09-15 16:24:10 +0900

    update1

---
+++ Prob_2588/Prob_2588/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void main() {
+	int val1 = 0;
+	string val2;
+
+	cin >> val1 >> val2;
+
+	int i_val2[3];
+
+	for (int i = 0; i < 3; i++) {
+		i_val2[i] = atoi((const char*)val2.at(i));
+	}
+
+	cout << 
+
+
+
+	
+}
