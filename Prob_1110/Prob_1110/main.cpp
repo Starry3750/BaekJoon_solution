8f80ad267e82411bb8b42b16eb6b528143f19f20
Starry3750 <49588390+Starry3750@users.noreply.github.com>
2020-09-16 22:34:15 +0900

    Clear Prob_1110

--- Prob_1110/Prob_1110/main.cpp
+++ Prob_1110/Prob_1110/main.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main() {
-	int val = 0, index = 0;
+	int val = 0, index = 1;
 	cin >> val;
 	
 	int new_val = val;
+	int front = 0, back = 0;
 
 	while (1) {
+		front = (new_val / 10);
+		back = (new_val % 10);
+		new_val = ((new_val % 10) * 10) + ((front + back) % 10);
+		
 		if (new_val == val)
 			break;
-		else {
-			int front = 0;
-			front = (val / 10);
-			new_val = ((val % 10) * 10) + front;
-		}
-		
-
+		else
+			index++;
 	}
 
-
+	cout << index << endl;
 	return 0;
 }
